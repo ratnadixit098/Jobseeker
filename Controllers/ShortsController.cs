f42a1e2934fe0f09d5425991dcd9a2d9a18be4ba
ratnadixist <ratnadixist@gmail.com>
2026-02-13 01:03:27 +0530

    Signed-off-by: ratnadixist <ratnadixist@gmail.com>

---
+++ Controllers/ShortsController.cs
@@ -0,0 +1,45 @@
+﻿using Jobseeker.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ShortsController : ControllerBase
+{
+    private readonly DbHelper _db;
+
+    public ShortsController(DbHelper db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public IActionResult GetShorts()
+    {
+        var list = new List<object>();
+
+        using (SqlConnection con = _db.GetConnection())
+        {
+            con.Open();
+            string query = @"
+                SELECT VideoId, Title, VideoUrl
+                FROM Video
+                WHERE CategoryId = 2";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                list.Add(new
+                {
+                    VideoId = dr["VideoId"],
+                    Title = dr["Title"],
+                    VideoUrl = dr["VideoUrl"]
+                });
+            }
+        }
+
+        return Ok(list);
+    }
+}
---
+++ Data/DbHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+namespace Jobseeker.Data
+{
+    public class DbHelper
+    {
+        private readonly string _connectionString;
+
+        public DbHelper(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
